d711e5a518e2348047482e4acb5d04e51c45ee42
Tim Schröder <56300785+timschr@users.noreply.github.com>
2022-02-17 14:34:02 +0100

    Update multi_hop_routing_kalin.c

--- project/multi_hop_routing/multi_hop_routing_kalin.c
+++ project/multi_hop_routing/multi_hop_routing_kalin.c
@@ -22,8 +22,6 @@ struct example_neighbor {
   struct ctimer ctimer;
 };
 
-static uint8_t sink_hops;
-
 
 static struct broadcast_conn broadcast;
 
@@ -38,9 +36,9 @@ PROCESS(broadcast_process, "broadcast");
 AUTOSTART_PROCESSES(&example_multihop_process, &broadcast_process);
 /*---------------------------------------------------------------------------*/
 
-static void broadcast_distance_to_sink()
+static void broadcast_distance_to_sink(uint8_t dist)
 {
-  packetbuf_copyfrom(&sink_hops, sizeof(sink_hops));
+  packetbuf_copyfrom(&dist, sizeof(dist));
   broadcast_send(&broadcast);
 }
 
@@ -58,6 +56,31 @@ remove_neighbor(void *n)
   memb_free(&neighbor_mem, e);
 }
 
+static struct example_neighbor* get_closest_neighbour(const linkaddr_t* skip)
+{
+  // Find the closest neighbor to the sink
+  struct example_neighbor *i, *closest;
+  for(i = list_head(neighbor_table), closest = i; i != NULL; i = i->next) {
+    if (closest->num_hops > i->num_hops && (!skip || (skip && !linkaddr_cmp(&i->addr, skip)))) {
+      closest = i;
+    }
+  }
+
+  return closest;
+}
+
+static uint8_t compute_shortest_path()
+{
+  struct example_neighbor *closest = get_closest_neighbour(NULL);
+
+  if (closest != NULL)
+  {
+    return closest->num_hops + 1;
+  }
+
+  return NOT_INIT;
+}
+
 /*---------------------------------------------------------------------------*/
 /*
  * This function is called when an incoming announcement arrives. The
@@ -69,29 +92,33 @@ remove_neighbor(void *n)
 static void
 received_broadcast(struct broadcast_conn *c, const linkaddr_t *from)
 {
+  if (linkaddr_cmp(&linkaddr_node_addr, &SINK_ADDRESS))
+  {
+    return;
+  }
+
   struct example_neighbor *e;
 
   uint8_t rec_number_of_hops = *(uint8_t *)packetbuf_dataptr();
 
   /* We received an announcement from a neighbor so we need to update
      the neighbor list, or add a new entry to the table. */
+
+  uint8_t current_shortest_path = compute_shortest_path();
+
   for(e = list_head(neighbor_table); e != NULL; e = e->next) {
     if(linkaddr_cmp(from, &e->addr)) {
       /* Our neighbor was found, so we update the timeout. */
       ctimer_set(&e->ctimer, NEIGHBOR_TIMEOUT, remove_neighbor, e); 
       if (rec_number_of_hops != NOT_INIT) {
-        if (sink_hops - 1 > rec_number_of_hops || sink_hops == NOT_INIT) {
-          sink_hops = rec_number_of_hops + 1;
-          e->num_hops = rec_number_of_hops;
-
-          broadcast_distance_to_sink();
-
-          printf("Updated #Hops to sink %d\n", sink_hops);
-          printf("sink_hops: %d, e->num_hops: %d, rec_number_of_hops: %d\n", sink_hops, e->num_hops, rec_number_of_hops);
+        e->num_hops = rec_number_of_hops;
+        if (current_shortest_path > rec_number_of_hops || current_shortest_path == NOT_INIT) {
+          broadcast_distance_to_sink(rec_number_of_hops + 1);
         }
-      } else {
-          printf("Neighbor %d doesn't know where sink is\n", from->u8[0]);
       }
+      // else {
+      //     printf("Neighbor %d doesn't know where sink is\n", from->u8[0]);
+      // }
       return;
     }
   }
@@ -101,22 +128,20 @@ received_broadcast(struct broadcast_conn *c, const linkaddr_t *from)
      necessary fields, and add it to the list. */
   e = memb_alloc(&neighbor_mem);
   if(e != NULL) {
-    if (rec_number_of_hops != NOT_INIT) {
-        if (sink_hops > rec_number_of_hops || sink_hops == NOT_INIT) {
-          sink_hops = rec_number_of_hops + 1;
-
-          broadcast_distance_to_sink();
-
-          printf("Updated #Hops to sinks %d\n", sink_hops);
-        }
-    } else {
-        printf("Neighbor %d not known yet, but doesn't know where sink is\n", from->u8[0]);
-    }
-
     e->num_hops = rec_number_of_hops;
     linkaddr_copy(&e->addr, from);
     list_add(neighbor_table, e);
     ctimer_set(&e->ctimer, NEIGHBOR_TIMEOUT, remove_neighbor, e);
+
+    if (rec_number_of_hops != NOT_INIT) {
+        if (current_shortest_path > rec_number_of_hops || current_shortest_path == NOT_INIT) {
+          broadcast_distance_to_sink(rec_number_of_hops + 1);
+        }
+    }
+    // else {
+    //     printf("Neighbor %d not known yet, but doesn't know where sink is\n", from->u8[0]);
+    // }
+
   }
 }
 
@@ -129,9 +154,9 @@ recv(struct multihop_conn *c, const linkaddr_t *sender,
      const linkaddr_t *prevhop,
      uint8_t hops)
 {
-  // TODO: Print the sender
-  printf("multihop message received from %d.%d: '%s'\n", sender->u8[0], sender->u8[1], (char*)packetbuf_dataptr());
+  printf("multihop message received from %d.%d: '%s' (# hops: %d)\n", sender->u8[0], sender->u8[1], (char*)packetbuf_dataptr(), hops);
 }
+
 /*
  * This function is called to forward a packet. The function picks a
  * random neighbor from the neighbor list and returns its address. The
@@ -144,15 +169,9 @@ forward(struct multihop_conn *c,
         const linkaddr_t *originator, const linkaddr_t *dest,
         const linkaddr_t *prevhop, uint8_t hops)
 {
-  // Find the closest neighbor to the sink
-  struct example_neighbor *i, *closest;
-  for(i = list_head(neighbor_table), closest = i; i != NULL; i = i->next) {
-    if (closest->num_hops > i->num_hops) {
-      closest = i;
-    }
-  }
+  struct example_neighbor* closest = get_closest_neighbour(prevhop);
 
-  if (closest != NULL && closest->num_hops != NOT_INIT) {
+  if (closest != NULL && closest->num_hops != NOT_INIT && !linkaddr_cmp(&closest->addr, prevhop)) {
     printf("%d.%d: Forwarding packet to %d.%d, hops %d\n",
              linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1],
              closest->addr.u8[0], closest->addr.u8[1],
@@ -176,13 +195,6 @@ PROCESS_THREAD(example_multihop_process, ev, data)
 
   /* Open a multihop connection on Rime channel MULTIHOP_CHANNEL. */
   multihop_open(&multihop, MULTIHOP_CHANNEL, &multihop_call);
-
-  if (linkaddr_cmp(&linkaddr_node_addr, &SINK_ADDRESS)){
-    sink_hops = 0;
-  }
-  else {
-    sink_hops = NOT_INIT;
-  }
   
   /* Activate the button sensor. We use the button to drive traffic -
      when the button is pressed, a packet is sent. */
@@ -194,7 +206,7 @@ PROCESS_THREAD(example_multihop_process, ev, data)
     PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event &&
                              data == &button_sensor);
 
-    /* Copy the "Hello" to the packet buffer. */
+    /* Copy the "EMERGENCY" to the packet buffer. */
     packetbuf_copyfrom("EMERGENCY", 10);
 
     /* Set the Rime address of the final receiver of the packet to
@@ -230,19 +242,13 @@ PROCESS_THREAD(broadcast_process, ev, data)
 
   // First node is the sink and thus its distance to the sink is 0
   if (linkaddr_cmp(&linkaddr_node_addr, &SINK_ADDRESS)){
-    sink_hops = 0;
-
     unsigned long ticks = CLOCK_SECOND * 5;
     etimer_set(&et, ticks);
 
     // Wait for all the nodes to be initialized
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
-    broadcast_distance_to_sink();
-  }
-  else {
-    // 
-    sink_hops = NOT_INIT;
+    broadcast_distance_to_sink(0);
   }
 
   // Broadcast distance to sink to neighbours 3 times more frequently than
@@ -252,7 +258,7 @@ PROCESS_THREAD(broadcast_process, ev, data)
     etimer_set(&et, NEIGHBOR_TIMEOUT / 3);
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
-    broadcast_distance_to_sink();
+    broadcast_distance_to_sink(compute_shortest_path());
   }
 
   PROCESS_END();
